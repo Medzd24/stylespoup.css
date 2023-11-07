e37485a120b00c94dd3b08719b77ee32c291bc38
Medzd24 <150199255+Medzd24@users.noreply.github.com>
2023-11-08 00:52:21 +0100

    Create stylespoup.js

---
+++ stylespoup.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+            if (window.location === window.parent.location) {
+                let alert = document.querySelector('#custom-alert');
+                let colors = ['#DC2626', '#0077B6', '#00A7E1'];
+                let colorIndex = 0;
+                setInterval(() => {
+                    alert.style.background = colors[colorIndex];
+                    colorIndex = (colorIndex + 1) % colors.length;
+                }, 2000);
+                setTimeout(() => {
+                    alert.style.display = 'block';
+                }, 8000);
+            }
+        });
